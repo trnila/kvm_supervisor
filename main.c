52969367c8d9d0c1f2f05990ea354e7c08e3efa5
Daniel Trnka <daniel.trnka@gmail.com>
2019-08-30 19:25:57 +0200

    fixed timing

--- main.c
+++ main.c
@@ -10,6 +10,10 @@
 #define FEEDBACK_PIN 12
 
 #define CPU_FREQ 8000000
+#define SYSTICK_PRESCALER 8
+
+#define BTN_INTERVAL_MS 280
+#define BTN_ACTIVE_MS 15
 
 void gpio_set_mode(GPIO_TypeDef *gpio, int pin, int cnf, int mode) {
   volatile uint32_t *reg = &gpio->CRL;
@@ -36,7 +40,7 @@ int get_gpio_index(GPIO_TypeDef *gpio) {
 
 void delay_ms(int delay) {
   SysTick->VAL = 0;
-  SysTick->LOAD = CPU_FREQ / 1000 * delay - 1;
+  SysTick->LOAD = CPU_FREQ / 1000 / SYSTICK_PRESCALER * delay - 1;
   SysTick->CTRL = SysTick_CTRL_ENABLE_Msk;
 
   while(!(SysTick->CTRL & (1 << 16)));
@@ -51,7 +55,7 @@ int main(void) {
 
   // setup btn gpio
   BTN_PORT->ODR |= 1 << BTN_PIN;
-  gpio_set_mode(BTN_PORT, BTN_PIN, 0b00 /* output push-pull */, 0b10 /* max speed 2 mhz*/);
+  gpio_set_mode(BTN_PORT, BTN_PIN, 0b01 /* output open-drain */, 0b10 /* max speed 2 mhz*/);
 
   // setup feedback gpio
   gpio_set_mode(FEEDBACK_PORT, FEEDBACK_PIN, 0b10 /* input */, 0b00);
@@ -74,18 +78,22 @@ int main(void) {
   // rising edge
   EXTI->RTSR |= 1 << DETECT_PIN;
 
+  // wait for controller power up
+  delay_ms(50);
 
   for(;;) {
-    // sleep until rising/falling edge on detect pin
-    asm("wfe");
     uint32_t feedback = (FEEDBACK_PORT->IDR & (1 << FEEDBACK_PIN)) > 0;
     uint32_t detect = (DETECT_PORT->IDR & (1 << DETECT_PIN)) > 0;
 
     if(detect != feedback) {
       BTN_PORT->ODR &= ~(1 << BTN_PIN);
-      delay_ms(20);
+      delay_ms(BTN_ACTIVE_MS);
       BTN_PORT->ODR |= 1 << BTN_PIN;
-      delay_ms(30);
+      delay_ms(BTN_INTERVAL_MS - BTN_ACTIVE_MS);
+    } else {
+      // sleep until rising/falling edge on detect pin
+      // debug is not clocked, so you have to pull BOOT0 high, reset and connect
+      asm("wfe");
     }
   }
 }
