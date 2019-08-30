82f20e3680fb47a68123b1ecfdb96beb2ce5a4a7
Daniel Trnka <daniel.trnka@gmail.com>
2019-08-30 10:34:25 +0200

    initial commit

---
+++ Makefile
@@ -0,0 +1,20 @@
+TARGET=kvm_switch.elf
+
+PREFIX = arm-none-eabi-
+CC = $(PREFIX)gcc
+AS = $(CC) -x assembler-with-cpp
+SZ = $(PREFIX)size
+CFLAGS = -ICMSIS -mcpu=cortex-m3 -mthumb -g
+LDFLAGS = -specs=nano.specs -TSTM32F103C6Tx_FLASH.ld -lc -lnosys
+
+all: $(TARGET)
+
+$(TARGET): main.o startup_stm32f103x6.o
+	$(CC) $^ $(LDFLAGS) -o $@
+	$(SZ) $@
+
+%.o: %.s
+	$(AS) -c $(CFLAGS) $< -o $@
+
+clean:
+	rm -f *.o $(TARGET)
---
+++ main.c
@@ -0,0 +1,91 @@
+#include "stm32f103xb.h"
+
+#define BTN_PORT GPIOB
+#define BTN_PIN 6
+
+#define DETECT_PORT GPIOB
+#define DETECT_PIN 11
+
+#define FEEDBACK_PORT GPIOA
+#define FEEDBACK_PIN 12
+
+#define CPU_FREQ 8000000
+
+void gpio_set_mode(GPIO_TypeDef *gpio, int pin, int cnf, int mode) {
+  volatile uint32_t *reg = &gpio->CRL;
+
+  if(pin > 7) {
+    pin -= 8;
+    reg = &gpio->CRH;
+  }
+  uint32_t offset = 4 * pin;
+
+  *reg &= ~(0xF << offset);
+  *reg |= ((cnf << 2) | mode) << offset;
+}
+
+int get_gpio_index(GPIO_TypeDef *gpio) {
+  GPIO_TypeDef *gpios[] = {GPIOA, GPIOB, GPIOC, GPIOD};
+  for(int i = 0; i < sizeof(gpios) / sizeof(*gpios); i++) {
+    if(gpios[i] == gpio) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void delay_ms(int delay) {
+  SysTick->VAL = 0;
+  SysTick->LOAD = CPU_FREQ / 1000 * delay - 1;
+  SysTick->CTRL = SysTick_CTRL_ENABLE_Msk;
+
+  while(!(SysTick->CTRL & (1 << 16)));
+  SysTick->CTRL = 0;
+}
+
+int main(void) {
+  // enable clocks
+  RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN;
+  RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+
+  // setup btn gpio
+  BTN_PORT->ODR |= 1 << BTN_PIN;
+  gpio_set_mode(BTN_PORT, BTN_PIN, 0b00 /* output push-pull */, 0b10 /* max speed 2 mhz*/);
+
+  // setup feedback gpio
+  gpio_set_mode(FEEDBACK_PORT, FEEDBACK_PIN, 0b10 /* input */, 0b00);
+  // enable pull-down resistor
+  FEEDBACK_PORT->BSRR = (1 << FEEDBACK_PIN) << 16U;
+
+  // setup detect gpio
+  gpio_set_mode(DETECT_PORT, DETECT_PIN, 0b10 /* input */, 0b00);
+  // enable pull-down resistor
+  DETECT_PORT->BSRR = (1 << DETECT_PIN) << 16U;
+
+  // enable interrupt request from pin on port
+  AFIO->EXTICR[DETECT_PIN / 4] &= ~(0xF << (4 * (DETECT_PIN & 0x3)));
+  AFIO->EXTICR[DETECT_PIN / 4] |= get_gpio_index(DETECT_PORT) << (4 * (DETECT_PIN & 0x3));
+
+  // enable event requests on gpio line - used with WFE instruction
+  EXTI->EMR |= 1 << DETECT_PIN;
+  // falling edge
+  EXTI->FTSR |= 1 << DETECT_PIN;
+  // rising edge
+  EXTI->RTSR |= 1 << DETECT_PIN;
+
+
+  for(;;) {
+    // sleep until rising/falling edge on detect pin
+    asm("wfe");
+    uint32_t feedback = (FEEDBACK_PORT->IDR & (1 << FEEDBACK_PIN)) > 0;
+    uint32_t detect = (DETECT_PORT->IDR & (1 << DETECT_PIN)) > 0;
+
+    if(detect != feedback) {
+      BTN_PORT->ODR &= ~(1 << BTN_PIN);
+      delay_ms(20);
+      BTN_PORT->ODR |= 1 << BTN_PIN;
+      delay_ms(30);
+    }
+  }
+}
